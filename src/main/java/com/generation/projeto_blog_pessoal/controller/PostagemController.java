f0fbbe14937b39a10a80b4e20f68433421e06c4e
jufaria <julio.faria@mercadolivre.com>
2023-02-01 15:03:13 -0300

    Exercicios CRUD parte 3

--- src/main/java/com/generation/projeto_blog_pessoal/controller/PostagemController.java
+++ src/main/java/com/generation/projeto_blog_pessoal/controller/PostagemController.java
@@ -2,6 +2,7 @@ package com.generation.projeto_blog_pessoal.controller;
 
 import com.generation.projeto_blog_pessoal.model.Postagem;
 import com.generation.projeto_blog_pessoal.repository.PostagemRepository;
+import com.generation.projeto_blog_pessoal.repository.TemaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,8 +17,12 @@ import java.util.Optional;
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
+
     @Autowired
     private PostagemRepository postagemRepository;
+
+    @Autowired
+    private TemaRepository temaRepository;
 
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll() {
@@ -28,8 +33,10 @@ public class PostagemController {
 
     @PostMapping
     public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-        return ResponseEntity.status(HttpStatus.CREATED)
+        if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
                 .body(postagemRepository.save(postagem));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
 
     }
 
@@ -47,10 +54,16 @@ public class PostagemController {
 
     @PutMapping
     public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-        return postagemRepository.findById(postagem.getId())
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
-                        .body(postagemRepository.save(postagem)))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+       if(postagemRepository.existsById(postagem.getId())) {
+
+           if(temaRepository.existsById(postagem.getTema().getId()))
+               return ResponseEntity.status(HttpStatus.OK)
+                       .body(postagemRepository.save(postagem));
+
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+       }
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
