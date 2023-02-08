0d7badb82153d368f38af6b6f12b40a98eee9cbe
jufaria <julio.faria@mercadolivre.com>
2023-02-08 15:50:48 -0300

    Exercicios security spring

--- src/main/java/com/generation/projeto_blog_pessoal/model/Tema.java
+++ src/main/java/com/generation/projeto_blog_pessoal/model/Tema.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @Entity
-@Table(name = "tb_temass")
+@Table(name = "tb_temas")
 public class Tema {
 
     @Id
---
+++ src/main/java/com/generation/projeto_blog_pessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.generation.projeto_blog_pessoal.security;
+
+import com.generation.projeto_blog_pessoal.model.Usuario;
+import com.generation.projeto_blog_pessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Optional<Usuario> usuario =usuarioRepository.findByUsuario(username);
+
+        if(usuario.isPresent())
+            return new UserDetailsImpl(usuario.get());
+        else
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    }
+
+
+}
